07291f1ddf4015bbb011acb30c719ec4013c17da
seokjin0706 <seokjin_0706@naver.com>
2022-12-07 16:30:46 +0900

    [ADD] 게시글 상세 보기

---
+++ Android/app/src/main/java/com/example/baedal_baedal/ShowWriteActivity.kt
@@ -0,0 +1,10 @@
+package com.example.baedal_baedal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowWriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
