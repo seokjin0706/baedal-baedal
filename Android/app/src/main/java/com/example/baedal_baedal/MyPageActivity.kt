bfd961ae7d49c645193b48cded1ef951c9478d8e
min-hyeok-Park <96771644+min-hyeok-Park@users.noreply.github.com>
2022-12-07 22:54:05 +0900

    [ADD] 마이페이지 기능 추가

--- Android/app/src/main/java/com/example/baedal_baedal/MyPageActivity.kt
+++ Android/app/src/main/java/com/example/baedal_baedal/MyPageActivity.kt
@@ -1,23 +1,51 @@
 package com.example.baedal_baedal
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MyPageActivity : AppCompatActivity() {
+    var PREFS_KEY = "prefs"
+    var adress_KEY = "adress"
+    var nickName_KEY = "nickName"
+
+    lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
 
+        val logoutBtn = findViewById<Button>(R.id.logoutBtn)
         val homeBtn = findViewById<Button>(R.id.homeBtn)
         val chatBtn = findViewById<Button>(R.id.chatBtn)
         val myPageBtn = findViewById<Button>(R.id.myPageBtn)
+        var nickTxt = findViewById<TextView>(R.id.nicknameTxt)
+        var addressTxt = findViewById<TextView>(R.id.addressTxt)
 
+        val loginIntent = Intent(this, MainActivity::class.java)
         val homeIntent = Intent(this, MainInterfaceActivity::class.java)
         val chatIntent = Intent(this, ChatActivity::class.java)
         val myPageIntent = Intent(this, MyPageActivity::class.java)
 
+        // nicknameTxt, addressTxt
+        sharedPreferences = getSharedPreferences(PREFS_KEY, Context.MODE_PRIVATE)
+        var nickName = sharedPreferences.getString(nickName_KEY, null)!!
+        var adress = sharedPreferences.getString(adress_KEY, null)!!
+
+        nickTxt.setText(nickName)
+        addressTxt.setText(adress)
+
+        logoutBtn.setOnClickListener {
+            Toast.makeText(this, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show()
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(loginIntent)
+        }
         homeBtn.setOnClickListener {
             startActivity(homeIntent)
         }
